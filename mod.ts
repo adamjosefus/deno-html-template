99d701e2420ef1d51267e9a0c1306e54c24352aa
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2021-08-25 14:12:59 +0200

    Create mod.ts

---
+++ mod.ts
@@ -0,0 +1,4 @@
+export { Template } from "./model/Template.ts"
+export { ContentPart } from "./model/ContentPart.ts"
+export { HtmlContentPart } from "./model/HtmlContentPart.ts"
+export { JsContentPart } from "./model/JsContentPart.ts"
